da5d851416934419458b21ff4e0b130561d8a4dd
Muhammad <muhammad@rededge.com>
2015-06-26 17:24:40 -0400

    Use jQuery to show/hide code segments

--- js/main.js
+++ js/main.js
@@ -11,14 +11,45 @@ $(document).ready(function(){
     //  On click, show the container and hide the currently active one
     $('.nav-tabs li').on('click',function(){
         //  Get the name of the class to target
-        var c = $(this).find('a').attr('class');
+        var select  = $(this).find('a').attr('class');
         var current = $('.active').find('a').attr('class');
 
-        if( c==current )
+        if( select==current )
             return;
 
         //  Change the selector
         $('.'+current).parent().removeClass('active');
-        $('.'+c).parent().addClass('active');
+        $('.'+select).parent().addClass('active');
+
+        //  Translate languages into their code equivalents
+        current = linkClassToCodeClass(current);
+        select  = linkClassToCodeClass(select);
+
+        //  Change the visible code
+        $('code[class="'+current+'"]').parent().parent().stop().hide(250,function(){
+            $('code[class="'+select+'"]').parent().parent().stop().show(250);
+        });
     });
+
+    function linkClassToCodeClass(name) {
+        var n = name;
+        switch( name ) {
+            default:
+                break;
+            case 'php':
+                n = 'language-php';
+                break;
+            case 'js':
+                n = 'language-javascript';
+                break;
+            case 'cs':
+                n = 'language-c#';
+                break;
+            case 'ror':
+                n = 'language-ruby';
+                break;
+        }
+
+        return n;
+    }
 });
