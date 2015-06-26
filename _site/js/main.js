2c50e53c8ee610a9bab42e67bf318ad7bb567d9f
Muhammad <muhammad@rededge.com>
2015-06-26 15:53:51 -0400

    Use JavaScript?

---
+++ _site/js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    //  Hide all code elements except for PHP
+    $('.language-javascript').hide();
+    $('.language-\\#').hide();
+    $('.language-ruby').hide();
+});
