78319a2d7aed0febc2f1fcd26eb3f6aafc6b5306
Muhammad <muhammad@rededge.com>
2015-06-26 16:58:30 -0400

    Try using jQ

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
 $(document).ready(function(){
+
+    var current = 'php';
+
     //  Hide all code elements except for PHP
     $('.language-javascript').parent().parent().hide();
     $('\\.language-\\#').parent().parent().hide();
     $('.language-ruby').parent().parent().hide();
+
+    //  On click, show the container and hide the currently active one
+    $('nav-tabs').find('a').on('click',function(){
+        alert( $(this).class() );
+    });
 });
